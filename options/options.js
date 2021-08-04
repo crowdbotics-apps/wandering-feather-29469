afdee269baafb140e38a2d439e80e25876d9634e
Crowdbotics <team@crowdbotics.com>
2021-08-04 19:13:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wandering_feather_29469Identifier",
+  url: "https://wandering_feather_29469Identifier.botics.co",
+  api: "https://wandering_feather_29469Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
